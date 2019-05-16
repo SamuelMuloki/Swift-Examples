c3d69594f1810367eced02576cfe71aaa1656e66
SamuelMuloki <samuellebron0@gmail.com>
2019-05-16 20:45:43 +0300

    Initial Commit

---
+++ Arrays/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Arrays
+//
+//  Created by samuel muloki on 16/05/2019.
+//  Copyright © 2019 line23. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
